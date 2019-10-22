837e865342d6df6d43b18dd743cb285d3056865f
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-22 09:50:31 +0200

    terzo esempio

---
+++ thread_solution_3/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace thread_solution_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<Thread>();
+
+            //Qui creiamo ed eseguiamo cinque worker thread 
+            for (int index=0; index<5; index++)
+            {
+                var myThread = new Thread((currentIndex) =>
+                {
+                    Console.WriteLine("Thtread {0} è iniziato", currentIndex);
+                    Thread.Sleep(500);
+                    Console.WriteLine("Thtread {0} è terminato", currentIndex);
+                });
+                myThread.Start(index);
+                list.Add(myThread);
+            }
+
+            //attesa del completamento di ognuno dei worker thread
+            foreach (Thread thread in list)
+            {
+                thread.Join();
+            }
+
+            Console.WriteLine("Esecuzione di tutti i thread terminata");
+
+            Console.ReadLine();
+        }
+    }
+}
