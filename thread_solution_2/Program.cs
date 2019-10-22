419bda86399e5fb7f9227454897f54e2bae72b2a
ITT Rosselli Rasetti <rosselli.rasetti@outlook.com>
2019-10-22 09:34:31 +0200

    secondo esempio

---
+++ thread_solution_2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace thread_solution_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string someVariable = "Matteo Tumiati";
+
+            var workerThread = new Thread((o) =>
+            {
+                Console.WriteLine("Saluti da: {0}", o);
+            });
+
+            workerThread.Start(someVariable);
+
+            
+
+            Console.ReadLine();
+        }
+    }
+}
